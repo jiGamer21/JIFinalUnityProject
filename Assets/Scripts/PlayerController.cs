8c7461a59ca920302d37765c25d6f69d52bb1fc3
james i <jisakson@student.fitchburgstate.edu>
2025-04-15 15:13:56 -0400

    updates

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 1f;
+    private float speed = 5f;
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-     
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");    
+        
+        rb.velocity = new Vector3(xInput, yInput, speed * Time.deltaTime);
+        /*float forwardForce = Time.deltaTime * speed * xInput;
+        float sidewaysForce = Time.deltaTime * speed * yInput;
+*/
         
     }
 }
